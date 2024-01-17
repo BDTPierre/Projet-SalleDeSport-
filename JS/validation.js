684ceb7e9a1a7d88ad8b8b863e0b55402da2c9aa
amine <devprogamine@gmail.com>
2024-01-17 17:11:52 +0100

    nav + slider

---
+++ JS/validation.js
@@ -0,0 +1,13 @@
+// const form = document.getElementById("form");
+// const submit = document.querySelector("button");
+ 
+
+// let data = "";
+// form.addEventListener("input", (e) => {
+//   //   e.preventDefault();
+//   data = e.target.value;
+//   document.body.innerHTML += data;
+// });
+
+
+// 
