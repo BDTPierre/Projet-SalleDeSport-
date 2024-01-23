545a9b28650796d6c4b966b64b218227ce0229cf
BDTPierre <buipierre@hotmail.fr>
2024-01-23 16:04:31 +0100

    Regex v2

--- JS/validation.js
+++ JS/validation.js
@@ -67,9 +67,10 @@ function addClass(element, regex, value) {
   
   tel.addEventListener("input", (e) => {
     let telnumber = e.target.value;
-    telnumber = inputValue.replace(/\s/g, '').replace(/^0/, '+33');
+    telnumber = telnumber.replace(/\s/g, '');
+    telnumber = telnumber.replace(/^0/, '+33');
 
-    addClass(tel, PhoneNumberRegex, e.target.value);
+    addClass(tel, PhoneNumberRegex, telnumber);
   
     if (tel.classList.contains("is-valid")) {
       telValid = true;
@@ -87,29 +88,26 @@ function addClass(element, regex, value) {
     }
   })
 
-  const form = document.querySelector('form');
+
+const submit = document.getElementsByClassName("form-contact")[0];
 // console.log(form);
 
 //On initie la fonction qui permettra d'écouter l'événement submit, lorsque l'utilisateur soumettra le formulaire
-form.addEventListener("submit", (e)=>{
+submit.addEventListener("submit", (e)=>{
     //on bloque la soumission automatique du formulaire
     e.preventDefault();
-
-    //Nous vérifions que chaque champs du formulaire soit correct, si tout est correct tu envoyer le mail 
+    // Nous vérifions que chaque champs du formulaire soit correct, si tout est correct tu envoyer le mail 
     if (nomValid && prenomValid  && emailValid && sujetValid && telValid && messageValid) {
         //Si tout les champs sont correct, le formulaire est soumis et un mail est envoyé
-            Email.send({
-                // Host : "smtp.elasticemail.com",
-                // Username : "pierrebui007@gmail.com",
-                // Password : "22B55B914616C042588C359AAB88BCB21EBB",
-                SecureToken : "837eee34-2787-47f3-ad16-53a0d40bf4bd", 
-                To : 'pierrebui007@gmail.com',
-                From : "pierrebui007@gmail.com",
-                Subject : "test",
-                Body : "J'espere que ca marche"
-            }). then(
-                message => alert(message)
-            );
+        Email.send({
+            SecureToken : "a0ab04df-34b0-4c8f-aaaf-8711b9a7a3c7",
+            To : 'pierrebui007@gmail.com',
+            From : "pierrebui007@gmail.com",
+            Subject : "test",
+            Body : "J'espere que ca marche",
+        }).then(
+          message => alert(message)
+        );
     } else {
         //Sinon le programme lance une alert pour demander à l'utilisateur de rentrer correctement les champs du formulaire 
         alert ("Tous les champs ne sont pas rentrés ou ne sont pas valide, mercie de rentrer correctement les champs du formulaire");
